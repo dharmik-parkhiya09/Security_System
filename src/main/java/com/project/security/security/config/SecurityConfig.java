e5b8f91003248ddb89c6188dc90860f76a11b29f
Dharmik Parakhiya <parakhiyadharmik09@gmail.com>
2026-03-08 18:52:40 +0530

    Fix root mapping to load index.html

--- src/main/java/com/project/security/security/config/SecurityConfig.java
+++ src/main/java/com/project/security/security/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**",
+                                "/",
                                 "/swagger-ui/**",
                                 "/favicon.ico",
                                 "/v3/api-docs/**",
